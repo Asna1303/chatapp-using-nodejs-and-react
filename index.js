caa8b8833b3df842cbe0bc281559edcd40ce830d
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-05 13:46:15 +0530

    Implemented asynchronous request to update user information using Axios

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+  try {
+    const r= await axios.put(
+       'https://api.chatengine.io/users/',
+       {username : usernamae, secret :username, first_name: username},
+       {headers:{"private-key" :"c8c7c9bf-3546-4912-b29c-9aceb5ebaebd"}}
+    )
+
+  } catch (e) {
+    
+  }
+
   return res.json({ username: username, secret: "sha256..." });
 });
 
