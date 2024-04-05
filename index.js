2350220108d9739106117698164ff905f6b01de9
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-05 14:52:44 +0530

    error corrected: imported axios library to send http request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios"); // Import axios library
 const cors = require("cors");
 
 const app = express();
@@ -8,10 +9,10 @@ app.use(cors({ origin: true }));
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
   try {
-    const r= await axios.put(
+    const r = await axios.put(
        'https://api.chatengine.io/users/',
-       {username : usernamae, secret :username, first_name: username},
-       {headers:{"private-key" :"c8c7c9bf-3546-4912-b29c-9aceb5ebaebd"}}
+       { username: username, secret: username, first_name: username },
+       { headers: { "private-key": "c8c7c9bf-3546-4912-b29c-9aceb5ebaebd" } }
     );
     return res.status(r.status).json(r.data);
 
